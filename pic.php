09ee6ba0e18274db5fccfd22c795ef64ff57ec5d
Yan Jiang <j582033@163.com>
2019-08-16 09:06:27 +0800

    输出改为使用php file_put_contents下载

--- pic.php
+++ pic.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Sunra\PhpSimple\HtmlDomParser;
 
 $url = "http://www.coloring-book.info/coloring/coloring_page.php?id=315";
+$save_path = "./files";
 $c = file_get_contents($url);
 
 $dom = HtmlDomParser::str_get_html( $c );
@@ -13,7 +14,15 @@ foreach($dom->find('img') as $img){
         preg_match('/^(.*)\/thumbs\/(.*_m.jpg)$/', $img_src, $match);
         $title = rawurlencode($match[1]);
         $pic = str_replace('_m', '', $match[2]);
-        echo "http://www.coloring-book.info/coloring/{$title}/{$pic}\n";
+        $url = "http://www.coloring-book.info/coloring/{$title}/{$pic}";
+        //获取图片data
+        $pic_content = file_get_contents($url);
+        //echo $pic_content;exit;
+        //拼装保存路径
+        $save_file = "{$save_path}/{$pic}";
+        echo "PIC: {$url} Downloading...\n";
+        //下载
+        file_put_contents($save_file, $pic_content);
     }
 }
 
